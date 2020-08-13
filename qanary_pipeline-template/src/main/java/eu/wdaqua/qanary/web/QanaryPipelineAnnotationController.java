bcb350212f875c64b8f050d1aeebf37df94358d9
p-Heinze <46108992+p-Heinze@users.noreply.github.com>
2020-08-13 16:22:09 +0200

    add controller class

---
+++ qanary_pipeline-template/src/main/java/eu/wdaqua/qanary/web/QanaryPipelineAnnotationController.java
@@ -0,0 +1,10 @@
+package eu.wdaqua.qanary.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * controller for Annotations created by compo
+ */
+@Controller
+public class QanaryPipelineAnnotationController {
+}
