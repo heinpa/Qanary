0957ae3af5f534acbc5576acf7bacd99dfc6c2c1
p-Heinze <46108992+p-Heinze@users.noreply.github.com>
2021-01-08 15:22:54 +0100

    fix(ui deployment): remove windows line endings
    run-app.sh was created on windows and used an inappropriate line ending.
    The dockerfile now includes a RUN to remove these line endings s.t.
    the container can start without problems.

--- qanary-configuration-frontend/run-app.sh
+++ qanary-configuration-frontend/run-app.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# set -o nounset \
- #    -o errexit \
-   #  -o verbose \
-    # -o xtrace
-
-# override environment variables if provided as argument
 if [ $# -ne 0 ]; then
     echo "-> overriding env with args ..."
     for var in "$@"
@@ -16,9 +10,6 @@ fi
 
 echo "-> ENV vars:"
 env | sort
-
-echo "-> User:"
-id
 
 echo "-> building app ..."
 npm run build --production
